b27d4b6e453d8d0ee310a1ea7587a65baae06595
Triston <tristonherbst97@gmail.com>
2021-03-22 17:15:06 -0500

    commit tree abb312d1338cde580509c58b2fad6be0c861ddab

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Student.destroy_all
+SchoolClass.destroy_all
+
+Student.create(first_name: "Triston", last_name: "Herbst")
+Student.create(first_name: "Devante", last_name: "L")
+Student.create(first_name: "Jose", last_name: "Mendoza")
+Student.create(first_name: "Nestor", last_name: "V")
+
+
+SchoolClass.create(title: "Phase 1", room_number: 12)
+SchoolClass.create(title: "Phase 2", room_number: 1)
+SchoolClass.create(title: "Phase 3", room_number: 2)
+SchoolClass.create(title: "Phase 4", room_number: 142)
+SchoolClass.create(title: "Phase 5", room_number: 126)
